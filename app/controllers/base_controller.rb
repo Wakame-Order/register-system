9b6d0392bdd25d57bd1c6799b3a7e1d2455870b1
shghdyji <sh.ghdyji@gmail.com>
2016-04-02 17:47:52 +0900

    update login logic

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -2,11 +2,22 @@ class BaseController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
 
-  def signed_in_user?
-    @user.present?
+  def user_signed_in?
+    session[:user_id].present?
   end
+  
   
   def registered_user?(account, password)
     User.find_by(account, password).present?
+  end
+
+  def user_session(user)
+    session[:user_id] = user.id
+  end
+
+  def get_user_id
+    if !session[:user_id].present?
+      session[:user_id]
+    end
   end
 end
--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -3,7 +3,31 @@ class LoginController < ApplicationController
 
   #TODO ログインメインロジック
   def login
-    #ログインと
+    if user_signed_in? 
+      #TODO top　ページの戻る仕組み
+    else
+      #TODO loginページへの遷移を促す
+    end
+    if registered_user?(user_params)
+      #userがログインしているかは
+      user_session(set_user(user_params))
+    end
+  end
+
+  def logout
+    if user_signed_in?
+      session.delete(:user_id)
+    end
+  end
+
+  private 
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def set_user account, password
+    user = User.find_by(:account, :password)
+  end
+
+  def user_params
+    params.require(:session).permit(:account, :password)
   end
 
 end
